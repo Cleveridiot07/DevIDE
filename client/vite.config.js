f09e12909ccafe00e8111d2fa333aa75b6c36406
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-09-14 17:23:41 +0530

    Containerzied the Project

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // Allows external access
+    port: 5173, // Default port
+  },
   plugins: [react()],
 })
